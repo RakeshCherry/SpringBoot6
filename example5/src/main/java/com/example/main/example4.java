f243152f5a76f34d78635420f1dffac13102d768
RAKESH M B <rakeshbhavimani1@gmail.com>
2025-02-22 13:54:52 +0530

    Bean creation using Component Annotation

---
+++ example5/src/main/java/com/example/main/example4.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import com.example.Config.ProjectConfig;
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class example4 {
+    public static void main(String[] args) {
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from spring context is: " + veh.getName());
+
+    }
+}
