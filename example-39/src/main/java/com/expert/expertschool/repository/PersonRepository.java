171885f6a0aaafe0740ecd6a3da306d25d3294f9
RAKESH M B <rakeshbhavimani1@gmail.com>
2025-03-21 17:26:44 +0530

    Password hashing using BCryptPasswordEncoder

---
+++ example-39/src/main/java/com/expert/expertschool/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.expert.expertschool.repository;
+
+import com.expert.expertschool.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    Person readByEmail(String email);
+
+}
