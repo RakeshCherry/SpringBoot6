23c75dc7bff3393af8e9d9c5c9a6d73275a47624
RAKESH M B <rakeshbhavimani1@gmail.com>
2025-03-24 14:41:15 +0530

    OneToMany, ManyToOne mapping demo using Classes, Students

---
+++ example-41/src/main/java/com/expert/expertschool/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.expert.expertschool.repository;
+
+import com.expert.expertschool.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+}
